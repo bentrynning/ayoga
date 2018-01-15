84f81aa31490e9b8d8f0d2c980245a71096eb871
Bent <bent.rynning@gmail.com>
2018-01-15 20:46:45 +0100

    Removed generated files from tracking, updated pages

--- cms/index.js
+++ cms/index.js
@@ -1,6 +1,6 @@
 {
-	name: 'Bent Test',
-	specialty: 'Demo page',
+	name: 'Authentic yoga',
+	specialty: 'Her kommer undertekst',
 	$main_image_type: 'image',
 	main_image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880097_juliesmith_low.jpg',
 	$background_type: 'textarea',
