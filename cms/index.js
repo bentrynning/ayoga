2c814596b6a9b0d53c1ee007899737d4eec51dea
Bent <bent.rynning@gmail.com>
2018-01-22 12:04:36 +0100

    Added more to content, new home page

--- cms/index.js
+++ cms/index.js
@@ -2,7 +2,7 @@
 	name: 'Authentic yoga',
 	specialty: 'Her kommer undertekst',
 	$main_image_type: 'image',
-	main_image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880097_juliesmith_low.jpg',
+	main_image: '/remote/direct_uploads/1516612170_2716x1810-1418992-yoga-hd-wallpaper.jpg',
 	$background_type: 'textarea',
 	background: 'I am a professional software developer with passion for deep learning. After finishing my computer science masters, I worked at Tim Dynamics at a autonomous submarine project. The amazing complexities of developing highly sofisticated diving model was astonishing. Sadly, after the submarine got stuck nearly 2 kilometers under the see surface, our funding was stopped. I became a freelance deep learning specialist and now enjoy diversity of projects ranging from medical research to video game AI.',
 	$projects_min: 1,
@@ -44,40 +44,18 @@
 			link: 'https://www.cs.cmu.edu/news/carnegie-mellon-project-uses-machine-learning-help-apartment-hunters-estimate-utility-costs'
 		}
 	],
-	$social_max: 3,
-	$social_min: 1,
-	social: [
-		{
-			$icon_type: 'fa',
-			icon: 'github',
-			text: 'Check out my code at github',
-			link: 'https://github.com/Gottwik/Enduro'
-		},
-		{
-			$icon_type: 'fa',
-			icon: 'linkedin',
-			text: 'View my profile at LinkedIn',
-			link: 'https://www.linkedin.com/'
-		},
-		{
-			$icon_type: 'fa',
-			icon: 'stack-overflow',
-			text: 'I am also active on stackoverflow',
-			link: 'http://stackoverflow.com/cv/martingottweis'
-		}
-	],
 	$hobby_background_type: 'image',
 	hobby_background: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880478_pexels-photo-92323.jpeg',
-	hobby_title: 'Book enthusiast',
-	hobby_subtitle: 'Books are the old-school internet',
+	hobby_title: 'Yoga enthusiast',
+	hobby_subtitle: 'Yoga is the art of living',
 	cta_tagline: 'Need a deep learning software developer?',
 	cta_text: 'I am actively looking for new challenges',
 	contact_email: 'contact@endurojs.com',
-	footer_text: '2017 @ yoga',
+	footer_text: '@ 2018 Authentic yoga',
 	$footer_background_color_type: 'colorpicker',
 	footer_background_color: '#25267e',
-	page_title: 'Mirror - enduro.js theme',
-	display_edit_stamp: true,
+	page_title: 'Authentic yoga',
+	display_edit_stamp: false,
 	$style_expander: true,
 	$style_info: 'won\'t work with the temp action',
 	style: {
@@ -90,5 +68,24 @@
 		$accent_color_size: 'inline',
 		$accent_color_type: 'colorpicker',
 		accent_color: '#fafdff'
-	}
+	},
+	$social_max: 3,
+	$social_min: 1,
+	social: [
+		{
+			$icon_type: 'fa',
+			icon: 'facebook',
+			link: 'https://github.com/Gottwik/Enduro'
+		},
+		{
+			$icon_type: 'fa',
+			icon: 'linkedin',
+			link: 'https://www.linkedin.com/'
+		},
+		{
+			$icon_type: 'fa',
+			icon: 'instagram',
+			link: 'http://stackoverflow.com/cv/martingottweis'
+		}
+	]
 }
