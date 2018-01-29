3e35e52dc526b6535092c18fde80ab8fb1b648be
Bent <bent.rynning@gmail.com>
2018-01-29 23:06:08 +0100

    Added navigation for mobile

--- assets/js/handlebars.js
+++ assets/js/handlebars.js
@@ -1,10 +1,9 @@
-define([
-	'handlebars_core'
-], function (__templating_engine) {
+import __templating_engine from 'handlebars_core';
+import hbs_helpers_loader from 'assets/hbs_helpers/hbs_helpers.js';
 
-	require(['assets/hbs_helpers/hbs_helpers.js'], function (hbs_helpers_loader) {
-		hbs_helpers_loader(__templating_engine)
-	})
+export default function(__templating_engine) {
 
-	return __templating_engine
-})
+    hbs_helpers_loader(__templating_engine);
+
+    return __templating_engine;
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+let state = {
+    isActive: false,
+}
 
+const init = () => {
+    const body = document.getElementsByTagName('body')[0];
+    const menu = document.getElementsByClassName('js-hamburger')[0];
+
+    menu.addEventListener('click', () => {
+        state.isActive = !state.isActive;
+        if (state.isActive) {
+            body.classList.add('nav-active');
+            menu.classList.add('is-active')
+        } else {
+            menu.classList.remove('is-active');
+            body.classList.remove('nav-active');
+        }
+    });
+
+};
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        init();
+    }
+};
